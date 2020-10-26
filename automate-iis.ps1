65a87f0f7dfd812eb01e6747a0137a6f3de85703
Tony Tang <tony.cn@outlook.com>
2020-10-26 14:50:08 +0800

    Update automate-iis.ps1

--- automate-iis.ps1
+++ automate-iis.ps1
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 Add-WindowsFeature Web-Server
-Set-Content -Path "C:\inetpub\wwwroot\Default.htm" -Value "Hello World from host $($env:computername), version 1.0"
+Set-Content -Path "C:\inetpub\wwwroot\index.html" -Value "Hello World from host $($env:computername), version 1.1"
